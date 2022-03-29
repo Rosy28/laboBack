daf60e434a05a1a925a5cea2113efa0fbf69f7c2
Rosy28 <rosycarmonauscanga@gmail.com>
2022-03-28 18:44:07 -0600

    Creating a pdf...

--- src/services/pdf.js
+++ src/services/pdf.js
@@ -5,11 +5,10 @@ const pool = require('../database/keys.js'),
 
 
 const createPdf = async (req, res) => {
-    const { idpacient, idpedid } = req.body;
+    //const { idpacient, idpedid } = req.body;
     
-    const response = await pool.query(queryPU, [idpacient, idpedid]);
-    const response2 = JSON.stringify(response);
-    const content = '<p>' + response2 + '<p/>';
+    //const response = await pool.query(queryPU, [idpacient, idpedid]);
+    const content = '<p> I´m trying to create a pdf...<p/>';
 
     pdf.create(content).toFile('./downloads/Report.pdf', function(err, res) {
         if(err){
