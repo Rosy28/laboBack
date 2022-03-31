c939c120897fe38599cc6f3e7fb5439594766443
Rosy28 <rosycarmonauscanga@gmail.com>
2022-03-31 13:05:48 -0600

    creating a pdf

--- src/services/pdf.js
+++ src/services/pdf.js
@@ -4,19 +4,5 @@ const pool = require('../database/keys.js'),
 
 
 
-const createPdf = async (req, res) => {
-    //const { idpacient, idpedid } = req.body;
-    
-    //const response = await pool.query(queryPU, [idpacient, idpedid]);
-    const content = '<p> I´m trying to create a pdf...<p/>';
 
-    pdf.create(content).toFile('C:/Users/corde/Desktop/Desarrollo/Labo/Report.pdf', function(err, res) {
-        if(err){
-            console.log('El error es: ' + err);
-        } else {
-            console.log(res);
-        }
-    });
-}
 
-module.exports = { createPdf };
