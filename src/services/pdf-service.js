6f99d3610a57c647fea9db034ed84099af1dc4e2
Rosy28 <rosycarmonauscanga@gmail.com>
2022-03-31 19:45:04 -0600

    finally creating a pdf

--- src/services/pdf-service.js
+++ src/services/pdf-service.js
@@ -1,7 +1,9 @@
 const PDFDocument = require('pdfkit');
 
-function buildPdf(){
+function buildPdf(dataCallback, endCallback){
     const doc = new PDFDocument();
+    doc.on('data', dataCallback);
+    doc.on('end', endCallback);
     doc.fontSize(25).text('Some heading');
     doc.end();
 }
