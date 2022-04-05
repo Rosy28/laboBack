49114c2fe514565c3c7cd9c17eb2b106e2cda300
Rosy28 <rosycarmonauscanga@gmail.com>
2022-04-05 17:26:18 -0500

    Modifying the response

--- src/services/pdf-service.js
+++ src/services/pdf-service.js
@@ -13,27 +13,14 @@ const buildPdf = async (req, res) => {
     doc.on('data', (chunk) => stream.write(chunk));
     doc.on('end', () => stream.end());
 
-    //const { idpacient, idpedid } = req.body;
+    const { idpacient, idpedid } = req.body;
 
-    //const response = await pool.query(queryPU, [idpacient, idpedid]);
+    const response = await pool.query(queryPU, [idpacient, idpedid]);
 
-    doc.fontSize(25).text('some heading');
+    doc.fontSize(25).text(JSON.stringify(response));
     doc.end();
 }
 
-
-
-const releasePdf = async (req, res, next) => {
-    const stream = res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment;filename=invoice.pdf'
-    });
-
-    buildPdf(
-        (chunk) => stream.write(chunk),
-        () => stream.end()
-    );
-}
 
 module.exports = { buildPdf };
 
